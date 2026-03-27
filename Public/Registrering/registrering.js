4563cdae37736e434bc77c243487bb58eaa0531c
BennyTheOverlord <benlodtzhauge@gmail.com>
2026-03-27 10:20:11 +0100

    Jobbet videre med prosjektarbeidet
    Endret en god del i strukturen på koden, og gjort den hakke mer oversiktlig.
    
    Har også gjort slik at brukeren nå kan trykke på en knapp for å legge til flere øvelser, med et h2 element som viser hvilke øvelser brukeren har lagt til så langt. I tillegg til at hver gang brukeren legger til en ny øvelse, lages det også 3 sett som brukeren kan fylle ut
    
    Har også fikset at submit knappen endte opp under settradene.

--- Public/Registrering/registrering.js
+++ Public/Registrering/registrering.js
@@ -1,32 +1,41 @@
 const minForm = document.getElementById("minForm")
+const leggTilØvelse = document.getElementById("leggTilØvelse");
 
 minForm.addEventListener("submit", async function(event) {
     event.preventDefault();
 })
 
-let brukerØvelseValg = document.getElementById("brukerØvelseValg");
+leggTilØvelse.addEventListener("click", () => {
+    lagØvelse();
+})
 
-
-
-hentData();
+function lagØvelse() {
+    hentData();
 
 async function hentData() {
     const response = await fetch("http://localhost:3000/api/Okt-registrering")
     const øvelseData = await response.json();
-    console.log(øvelseData); 
-    
+    console.log(øvelseData);
+
+    let øvelsesBlokk = document.createElement("div");
+    let overskrift = document.createElement("h2");
+    let settContainer = document.createElement("div"); 
+    let brukerØvelseValg = document.createElement("select");
+   
     for (const Navn of øvelseData) {
     const option = document.createElement("option");
     option.value = Navn.Navn;
     option.textContent = Navn.Navn;
     brukerØvelseValg.appendChild(option);
-    console.log(Navn.Navn)
+  }
+        øvelsesBlokk.append(overskrift, settContainer, brukerØvelseValg);
+        minForm.appendChild(øvelsesBlokk)
+ 
+ brukerØvelseValg.addEventListener("change", () => {
+    settContainer.innerHTML = "";
+    
+    overskrift.textContent = brukerØvelseValg.value
 
-   
-}
-
-brukerØvelseValg.addEventListener("change", () => {
-    minForm.innerHTML = "";
     for (let i = 1; i <= 3; i = i + 1) {
         let div = document.createElement("div");
         let settnr = document.createElement("span");
@@ -34,16 +43,65 @@ brukerØvelseValg.addEventListener("change", () => {
         let reps = document.createElement("input");
         
         settnr.textContent = i;
-        
         vekt.type = "number";
         vekt.placeholder = "Kg";
-
         reps.type = "number";
         reps.placeholder = "Reps";
 
         div.append(settnr, vekt, reps); // Hva er forskjellen på variabel.append(); og variabel.appendchild(); ?
-        minForm.appendChild(div);
-
+        settContainer.appendChild(div);
         }
-    });
+        
+     });
+    }
 }
+
+ 
+
+
+
+// hentData();
+
+// async function hentData() {
+//     const response = await fetch("http://localhost:3000/api/Okt-registrering")
+//     const øvelseData = await response.json();
+//     console.log(øvelseData); 
+    
+//     for (const Navn of øvelseData) {
+//     const option = document.createElement("option");
+//     option.value = Navn.Navn;
+//     option.textContent = Navn.Navn;
+//     brukerØvelseValg.appendChild(option);
+//     console.log(Navn.Navn)
+
+   
+// }
+
+// brukerØvelseValg.addEventListener("change", () => {
+//     settContainer.innerHTML = "";
+
+//     overskrift = document.createElement("h2");
+//     overskrift.innerHTML = brukerØvelseValg.value
+//     øvelseOverskrift.appendChild(overskrift);
+
+//     for (let i = 1; i <= 3; i = i + 1) {
+//         let div = document.createElement("div");
+//         let settnr = document.createElement("span");
+//         let vekt = document.createElement("input");
+//         let reps = document.createElement("input");
+        
+//         settnr.textContent = i;
+        
+//         vekt.type = "number";
+//         vekt.placeholder = "Kg";
+
+//         reps.type = "number";
+//         reps.placeholder = "Reps";
+
+//         div.append(settnr, vekt, reps); // Hva er forskjellen på variabel.append(); og variabel.appendchild(); ?
+//         settContainer.appendChild(div);
+
+//         }
+//     });
+// }
+
