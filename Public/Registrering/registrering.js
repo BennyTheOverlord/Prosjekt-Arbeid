c28633c659c7996d6321cf75558d76a00542c9be
BennyTheOverlord <benlodtzhauge@gmail.com>
2026-04-15 19:52:48 +0200

    Kommentert deler av JS koden

--- Public/Registrering/registrering.js
+++ Public/Registrering/registrering.js
@@ -1,36 +1,36 @@
-const minForm = document.getElementById("minForm");
-const leggTilØvelse = document.getElementById("leggTilØvelse");
+const minForm = document.getElementById("minForm"); // Lager en konstant variabel: minForm som html elementet med id-en "minForm"
+const leggTilØvelse = document.getElementById("leggTilØvelse"); // Lager en konstant variabel: leggTilØvelse som html elementet "leggTilØvelse"
 
 
 
 
-minForm.addEventListener("submit", async function(event) {
-    event.preventDefault();
+minForm.addEventListener("submit", async function(event) { // Legger til en eventlistener med typen submit til minForm, med en async funksjon
+    event.preventDefault(); // Hindrer siden i å refreshe når brukeren submiter, slik at dataen ikke går tapt
 
-const alleØvelser = document.querySelectorAll(".øvelses-Blokk");
+const alleØvelser = document.querySelectorAll(".øvelses-Blokk"); // Definerer en konstant vartiabel: alleØvelser som alle elementer med klassen "øvelses-Blokk"
 
-    const registrertData = {
+    const registrertData = { // Lager et tomt objekt slik at jeg kan pushe data om brukerens treningsøkt i den senere
     øvelser: []
 }
     
-    alleØvelser.forEach(blokk => {
-    const valgtØvelse = blokk.querySelector(".øvelse-Valg").value;
-    const alleSett = blokk.querySelectorAll(".sett-Rad");
+    alleØvelser.forEach(blokk => { // Går gjennom alle øvelsesblokkene og definerer valgt øvelse og allesett
+    const valgtØvelse = blokk.querySelector(".øvelse-Valg").value; // Definerer valgt øvelse som verdien brukeren valgte i elementet med klassen "øvelse-Valg"
+    const alleSett = blokk.querySelectorAll(".sett-Rad"); // Definerer allesett som alle elementene med klassen "sett-Rad"
 
-    const settArray = [];
+    const settArray = []; // Lager en array som jeg senere kan pushe informasjonen i settene i
 
-        alleSett.forEach(rad => {
-    const vekt = rad.querySelector(".vekt-Input").value;
-    const reps = rad.querySelector(".reps-Input").value;
+        alleSett.forEach(rad => { // Går gjennom alle elementene med klassen "sett-Rad" og definerer vekt og reps
+    const vekt = rad.querySelector(".vekt-Input").value; // Definerer vekt som verdien til elementet med klassen "vekt-Input"
+    const reps = rad.querySelector(".reps-Input").value; // Definerer reps som verdien til elementet med klassen "reps-Input"
 
-    if (vekt !== "" && reps !== "") {
+    if (vekt !== "" && reps !== "") { // Sjekker at vekt og reps ikke er tomme, og pusher da verdien deres til settarray
         settArray.push({
             vekt: Number(vekt),
             reps: Number(reps)
         })
     }
 });
-    if (settArray.length > 0) {
+    if (settArray.length > 0) { // Hvis det er noe data i settArray så pushes dataen til registrertData.øvelser
         registrertData.øvelser.push({
         navn: valgtØvelse,
         sett: settArray
@@ -40,9 +40,9 @@ const alleØvelser = document.querySelectorAll(".øvelses-Blokk");
    
 });
 
-console.log(registrertData);
+console.log(registrertData); // Logger registrertData for å sjekke at dataen fungerer som den skal
 
-const response = await fetch("/api/registrer_okt", {
+const response = await fetch("/api/registrer_okt", { // Poster dataen til databasen, og gjør informasjonen lagret i registrertData om til JSON format slik at det kan leses på backend
     method: "POST",
     headers: {
         "Content-Type": "application/json"
@@ -54,14 +54,14 @@ const response = await fetch("/api/registrer_okt", {
 
 })
 
-leggTilØvelse.addEventListener("click", () => {
+leggTilØvelse.addEventListener("click", () => { // Legger til enn eventlistener på typen "click" og kaller da på funksjonen lagØvelse();
     lagØvelse();
 })
 
-function lagØvelse() {
-    hentData();
+function lagØvelse() { // Definerer en skjult funksjon ved navn lagØvelse
+    hentData(); // Kaller funksjonen hentData
 
-async function hentData() {
+async function hentData() { // Definerer en async funksjon som henter dataen fra api-en, og gjør den om til et JS objekt
     const response = await fetch("http://localhost:3000/api/Okt-registrering")
     const øvelseData = await response.json();
     console.log(øvelseData);
